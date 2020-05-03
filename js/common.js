790979636fd3ca376f0568e4967a814187633397
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-05-03 12:35:40 +0300

    people slider

--- js/common.js
+++ js/common.js
@@ -3,6 +3,23 @@ $(document).ready(function() {
 	$('.pavilion__gallery').slick({
 		slidesToShow: 3
 	});
+
+
+	$('.people__info-slider').slick({
+		autoplay: true,
+		slidesToShow: 1,
+		fade: true,
+		asNavFor: $('.people__image-carousel'),
+	});
+
+	$('.people__image-carousel').slick({
+		autoplay: true,
+		slidesToShow: 1,
+		fade: true,
+		arrows: false,
+		asNavFor: $('.people__info-slider'),
+
+	});
 		
 		
 
