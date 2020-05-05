9d70c644859dfa3b4a3de75c6a96867439da126e
Alexandr <33997516+alexwebdev96@users.noreply.github.com>
2020-05-05 19:23:42 +0300

    mobile

--- js/common.js
+++ js/common.js
@@ -1,7 +1,29 @@
 $(document).ready(function() {
+
 	
+
+	$('.menu-init').on('click', function(e) {
+		e.preventDefault();
+		if ($('.menu').hasClass('menu_visible')){
+			$('.menu').removeClass('menu_visible');
+		} else {
+			$('.menu').toggleClass('menu_visible');
+		}
+		
+	});	
+	
+	$('.menu .menu-item').on('click', function() {
+		$('.menu').removeClass('menu_visible');
+	});
+
 	$('.pavilion__gallery').slick({
-		slidesToShow: 3
+		slidesToShow: 3,
+		responsive: [{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 1
+			}
+		}]
 	});
 
 
@@ -16,7 +38,7 @@ $(document).ready(function() {
 		autoplay: true,
 		slidesToShow: 1,
 		fade: true,
-		arrows: false,
+		arrows: true,
 
 	});
 
@@ -37,7 +59,30 @@ $(document).ready(function() {
 		$('body').removeClass('no-scroll');
 	});
 
-	
+	$('.scroll').mPageScroll2id();
 		
 
+	
+
+	$('.pavilion__gallery, .portfolio__content').magnificPopup({
+		delegate: 'a', // child items selector, by clicking on it popup will open
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true
+			
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+	});
 });
